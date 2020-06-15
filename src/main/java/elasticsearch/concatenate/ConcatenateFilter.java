2cc364356c39e097b5b40d5e1e86d1b127428b20
Reinhard Holzner <reinhard@smint.io>
2020-06-15 12:38:27 +0200

    Support ElasticSearch 7.7.1

--- src/main/java/elasticsearch/concatenate/ConcatenateFilter.java
+++ src/main/java/elasticsearch/concatenate/ConcatenateFilter.java
@@ -11,58 +11,59 @@ import org.apache.lucene.util.Version;
 
 public final class ConcatenateFilter extends TokenFilter {
 
-    private final static String DEFAULT_TOKEN_SEPARATOR = " ";
+	private final static String DEFAULT_TOKEN_SEPARATOR = " ";
 
-    private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
-    private final PositionIncrementAttribute posIncrAtt = addAttribute(PositionIncrementAttribute.class);
-    private String tokenSeparator = null;
-    private int incrementGap = 100;
-    private StringBuilder builder = new StringBuilder();
-    private AttributeSource.State previousState = null;
-    private boolean recheckPrevious = false;
+	private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+	private final PositionIncrementAttribute posIncrAtt = addAttribute(PositionIncrementAttribute.class);
+	private String tokenSeparator = null;
+	private int incrementGap = 100;
+	private StringBuilder builder = new StringBuilder();
+	private AttributeSource.State previousState = null;
+	private boolean recheckPrevious = false;
 
-    public ConcatenateFilter(TokenStream input, String tokenSeparator, int incrementGap) {
-        super(input);
-        this.tokenSeparator = tokenSeparator!=null ? tokenSeparator : DEFAULT_TOKEN_SEPARATOR;
-        this.incrementGap = incrementGap;
-    }
-
-    @Override
-    public boolean incrementToken() throws IOException {
-        boolean empty = false;
-        builder.setLength(0);
-
-	if(recheckPrevious) {
-	   restoreState(previousState);
-	   // append the term of the current token
-           builder.append(termAtt.buffer(), 0, termAtt.length());
-           recheckPrevious = false;
+	public ConcatenateFilter(TokenStream input, String tokenSeparator, int incrementGap) {
+		super(input);
+		this.tokenSeparator = tokenSeparator != null ? tokenSeparator : DEFAULT_TOKEN_SEPARATOR;
+		this.incrementGap = incrementGap;
 	}
 
-        while (input.incrementToken()) {
-	    if(posIncrAtt.getPositionIncrement() <= incrementGap) {
-                if (builder.length()>0) {
-                    // append the token separator
-                    builder.append(tokenSeparator);
-                }
-                // append the term of the current token
-                builder.append(termAtt.buffer(), 0, termAtt.length());
-            } else {
-		// we have found a new element in the array, the next token should start from this one
-                recheckPrevious = true;
-		previousState = captureState();
-	        break;
-	    }
+	@Override
+	public boolean incrementToken() throws IOException {
+		boolean empty = false;
+		builder.setLength(0);
+
+		if (recheckPrevious) {
+			restoreState(previousState);
+			// append the term of the current token
+			builder.append(termAtt.buffer(), 0, termAtt.length());
+			recheckPrevious = false;
+		}
+
+		while (input.incrementToken()) {
+			if (posIncrAtt.getPositionIncrement() <= incrementGap) {
+				if (builder.length() > 0) {
+					// append the token separator
+					builder.append(tokenSeparator);
+				}
+				// append the term of the current token
+				builder.append(termAtt.buffer(), 0, termAtt.length());
+			} else {
+				// we have found a new element in the array, the next token should start from
+				// this one
+				recheckPrevious = true;
+				previousState = captureState();
+				break;
+			}
+		}
+
+		if (builder.length() > 0) {
+			termAtt.setEmpty().append(builder);
+			if (!recheckPrevious) {
+				empty = true;
+			}
+		}
+
+		return empty;
 	}
-
-        if (builder.length()>0) {
-            termAtt.setEmpty().append(builder);
-	    if(!recheckPrevious) {
-	        empty = true;
-	    }
-        }
-
-        return empty;
-    }
 
 }
